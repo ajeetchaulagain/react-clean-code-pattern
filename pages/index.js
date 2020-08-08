191d28482e09fa1400a4a9545ef864b9c204d9af
ajeetchaulagain <chaulagainajeet@gmail.com>
2020-08-08 18:35:52 +1000

    inital commit. Next.js setup

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+const index = () => {
+  return (
+    <div>
+      <h2>Hello</h2>
+      <p>
+        Lets learn better data management in react using Context API combined
+        with custom hooks
+      </p>
+    </div>
+  );
+};
+
+export default index;
